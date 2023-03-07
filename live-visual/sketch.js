17dc6dffe324943da213d71a865af72ac9015fe5
Andrew Chuah <warlordpluto@gmail.com>
2023-03-07 12:46:36 -0500

    stuff

--- live-visual/sketch.js
+++ live-visual/sketch.js
@@ -10,42 +10,60 @@ The song is divided into 3 acts
 
 let crown,
   crownX = 1,
-  crownY = 1;
+  crownY = 1,
+  crownA;
 let bgm;
+let bgR = 111,
+  bgG = 156,
+  bgB = 186;
 
 let easing = 0.05;
 let rippleArray = [];
+let pastelColors = [
+  "#fdfd96",
+  "#77dd77",
+  "#ff6961",
+  "#84b6f4",
+  "#fdcae1",
+  "#b0c2f2",
+  "#ffda9e",
+  "#f6d1de",
+  "#eb9cff",
+  "#5086c1",
+  "#e3b1c8",
+  "#dfcae1",
+  "#ecd6c0",
+];
 
 class PulsingCircle {
-  constructor(x, y, r, g, b) {
+  constructor(x, y, hex, circleFade, circleSize, rippleFade, rippleSize) {
     this.x = x;
     this.y = y;
-    this.r = r;
-    this.g = g;
-    this.b = b;
+    this.hex = color(hex);
 
     this.circleA = 255;
     this.rippleA = 255;
-    this.circleFade = -15;
-    this.rippleFade = -12;
+    this.circleFade = circleFade; // default -15
+    this.rippleFade = rippleFade; // default -12
 
-    this.done = false;
-    this.circleSize = 100;
+    this.circleSize = circleSize;
+    this.rippleSize = rippleSize;
   }
 
   update() {
     this.circleA += this.circleFade;
+    this.hex.setAlpha(this.circleA);
     this.rippleA += this.rippleFade;
-    this.circleSize += 10;
+    this.rippleSize += 10;
   }
 
   display() {
     noStroke();
-    fill(this.r, this.g, this.b, this.circleA);
-    circle(this.x, this.y, 100);
+    fill(this.hex);
+    circle(this.x, this.y, this.circleSize);
     stroke(0, 64, 128, this.rippleA);
     noFill();
-    circle(this.x, this.y, this.circleSize * 0.75);
+    circle(this.x, this.y, this.rippleSize * 0.85);
   }
 }
 
@@ -69,15 +87,30 @@ function setup() {
   noCursor();
   noFill();
   strokeWeight(1);
+  crown.position(-100, 0);
 
   if (bgm.isLoaded && !bgm.isPlaying()) bgm.play();
 }
 
 // Act as update function to change based on song duration.
-
 function draw() {
-  background(111, 156, 186);
+  background(bgR, bgG, bgB);
 
+  // Fade to white
+  if (bgm.currentTime() >= 110) {
+    // set to 110
+    if (bgR < 250) {
+      bgR += 2;
+    }
+    if (bgG < 249) {
+      bgG += 2;
+    }
+    if (bgB < 246) {
+      bgB += 2;
+    }
+  }
+
+  // Shows pulsing circles
   for (let i = 0; i < rippleArray.length; i++) {
     let r = rippleArray[i];
     r.update();
@@ -85,20 +118,23 @@ function draw() {
   }
 
   // Floating crown
-  if (bgm.duration() >= 800) {
+  if (bgm.currentTime() >= 244) {
     crown.show();
     crownFloat();
   }
 }
 
+// Creates a new ripple when clicked
 function mousePressed() {
-  let ripple = new PulsingCircle(
-    mouseX,
-    mouseY,
-    random(255),
-    random(255),
-    random(200)
-  );
+  let c = random(pastelColors);
+  let ripple;
+  if (bgm.currentTime() <= 111) {
+    // set 1 second after 110
+    ripple = new PulsingCircle(mouseX, mouseY, c, -15, 100, -12, 100);
+  }
+  if (bgm.currentTime() > 111 && bgm.currentTime() <= 244) {
+    ripple = new PulsingCircle(mouseX, mouseY, c, -12, 200, -6, 200);
+  }
   rippleArray.push(ripple);
 }
 
