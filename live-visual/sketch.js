9f943e79a3935b36c13654a82841d14e8dae1ed6
Andrew Chuah <warlordpluto@gmail.com>
2023-03-07 10:38:22 -0500

    pulsing circles + crown

--- live-visual/sketch.js
+++ live-visual/sketch.js
@@ -8,8 +8,46 @@ The song is divided into 3 acts
 
 */
 
-let crown;
+let crown,
+  crownX = 1,
+  crownY = 1;
 let bgm;
+
+let easing = 0.05;
+let rippleArray = [];
+
+class PulsingCircle {
+  constructor(x, y, r, g, b) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+
+    this.circleA = 255;
+    this.rippleA = 255;
+    this.circleFade = -15;
+    this.rippleFade = -12;
+
+    this.done = false;
+    this.circleSize = 100;
+  }
+
+  update() {
+    this.circleA += this.circleFade;
+    this.rippleA += this.rippleFade;
+    this.circleSize += 10;
+  }
+
+  display() {
+    noStroke();
+    fill(this.r, this.g, this.b, this.circleA);
+    circle(this.x, this.y, 100);
+    stroke(0, 64, 128, this.rippleA);
+    noFill();
+    circle(this.x, this.y, this.circleSize * 0.75);
+  }
+}
 
 function preload() {
   soundFormats("mp3");
@@ -21,20 +59,60 @@ function preload() {
   // Load dream sequence assets
 
   // Load present assets
-
-  // Load song as bgm
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  imageMode(CENTER);
+  crown.hide();
+  noCursor();
+  noFill();
+  strokeWeight(1);
+
+  if (bgm.isLoaded && !bgm.isPlaying()) bgm.play();
 }
 
 // Act as update function to change based on song duration.
 
 function draw() {
   background(111, 156, 186);
-  crown.position(width / 2, height / 2);
-  crown.size(width / 5, height / 5);
+
+  for (let i = 0; i < rippleArray.length; i++) {
+    let r = rippleArray[i];
+    r.update();
+    r.display();
+  }
+
+  // Floating crown
+  if (bgm.duration() >= 800) {
+    crown.show();
+    crownFloat();
+  }
+}
+
+function mousePressed() {
+  let ripple = new PulsingCircle(
+    mouseX,
+    mouseY,
+    random(255),
+    random(255),
+    random(200)
+  );
+  rippleArray.push(ripple);
+}
+
+function crownFloat() {
+  let targetX = mouseX;
+  let dx = targetX - crownX;
+  crownX += dx * easing;
+
+  let targetY = mouseY;
+  let dy = targetY - crownY;
+  crownY += dy * easing;
+
+  crown.size(70, 70);
+  crown.position(crownX, crownY);
 }
 
 function Act1() {}
