509d26262828183eefa73dd46922bbd772415973
Andrew Chuah <warlordpluto@gmail.com>
2023-04-04 11:59:29 -0400

    capture

--- image-filter/sketch.js
+++ image-filter/sketch.js
@@ -1,19 +1,43 @@
-let img, img2, img3, img4, ui;
+let ui;
+let numFrames = 1,
+  whichFrame = 0,
+  frames,
+  capture;
 
 function preload() {
-  img = loadImage("./assets/Sunset.jpg");
-  img2 = loadImage("./assets/xd.jpg");
-  img3 = loadImage("./assets/astro.jpg");
-  img4 = loadImage("./assets/space.jpg");
+  frames = [];
+  for (let i = 1; i <= numFrames; i++) {
+    let filename = "input/" + nf(i, 6) + ".png";
+    let frame = loadImage(filename);
+    frames.push(frame);
+  }
+
   ui = loadImage("./assets/TikTokOverlay.png");
 }
 
 function setup() {
-  createCanvas(img3.width, img3.height);
-  //duotone(img, color("#98eb34"), color("#3471eb"));
-  filterSystemOne(img4);
-  //filterSystemTwo(img3);
-  //image(ui, 0, 0);
+  let canvas = createCanvas(1080, 1920);
+  canvas.id("canvas");
+
+  capture = new CCapture({
+    format: "png",
+    name: "output",
+  });
+}
+
+function draw() {
+  if (whichFrame === 0) capture.start();
+  console.log("- " + whichFrame + "/" + frames.length);
+
+  // overlay
+  image(ui, 0, 0);
+  capture.capture(document.getElementById("canvas"));
+  whichFrame++;
+  if (whichFrame === frames.length) {
+    capture.stop();
+    capture.save();
+    noLoop();
+  }
 }
 
 function threshold(img, cutoff) {
@@ -21,16 +45,24 @@ function threshold(img, cutoff) {
 
   for (let i = 0; i < img.height; i++) {
     for (let j = 0; j < img.width; j++) {
-      let px = img.get(j, i);
-      let r = px[0];
-      let g = px[1];
-      let b = px[2];
+      let index = (i * img.width + j) * 4;
+
+      let r = img.pixels[index];
+      let g = img.pixels[index + 1];
+      let b = img.pixels[index + 2];
 
       let bright = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
       //? Threshold
-      if (bright < cutoff) img.set(j, i, color(0));
-      else img.set(j, i, color(255));
+      if (bright < cutoff) {
+        img.pixels[index] = 0;
+        img.pixels[index + 1] = 0;
+        img.pixels[index + 2] = 0;
+      } else {
+        img.pixels[index] = 255;
+        img.pixels[index + 1] = 255;
+        img.pixels[index + 2] = 255;
+      }
     }
   }
 
@@ -42,13 +74,18 @@ function duotone(img, light, dark) {
 
   for (let i = 0; i < img.height; i++) {
     for (let j = 0; j < img.width; j++) {
-      let px = img.get(j, i);
-      let r = px[0];
-      let g = px[1];
-      let b = px[2];
+      let index = (i * img.width + j) * 4;
+
+      let r = img.pixels[index];
+      let g = img.pixels[index + 1];
+      let b = img.pixels[index + 2];
 
       let bright = map(0.2126 * r + 0.7152 * g + 0.0722 * b, 0, 255, 0, 1);
-      img.set(j, i, lerpColor(light, dark, bright));
+
+      let lerp = lerpColor(light, dark, bright);
+      img.pixels[index] = lerp.levels[0];
+      img.pixels[index + 1] = lerp.levels[1];
+      img.pixels[index + 2] = lerp.levels[2];
     }
   }
 
@@ -60,39 +97,40 @@ function tritone(img, color1, color2, color3) {
 
   for (let i = 0; i < img.height; i++) {
     for (let j = 0; j < img.width; j++) {
-      let px = img.get(j, i);
-      let r = px[0];
-      let g = px[1];
-      let b = px[2];
+      let lerp;
+      let index = (i * img.width + j) * 4;
+
+      let r = img.pixels[index];
+      let g = img.pixels[index + 1];
+      let b = img.pixels[index + 2];
 
       let bright = map(0.2126 * r + 0.7152 * g + 0.0722 * b, 0, 255, 0, 1);
-      if (bright > 0.5) img.set(j, i, lerpColor(color1, color2, bright));
-      else img.set(j, i, lerpColor(color2, color3, bright));
+
+      if (bright > 0.5) lerp = lerpColor(color1, color2, bright);
+      else lerp = lerpColor(color2, color3, bright);
+
+      img.pixels[index] = lerp.levels[0];
+      img.pixels[index + 1] = lerp.levels[1];
+      img.pixels[index + 2] = lerp.levels[2];
     }
   }
 
   img.updatePixels();
 }
 
-function halftone(
-  img,
-  constrainR,
-  constrainG,
-  constrainB,
-  constrainHeight,
-  minSize,
-  maxSize,
-  step
-) {
+function halftone(img, constraints, minSize, maxSize, step) {
   img.loadPixels();
   for (let i = 0; i < img.height; i += step) {
     for (let j = 0; j < img.width; j += step) {
-      let px = img.get(j, i);
-      let r = px[0];
-      let g = px[1];
-      let b = px[2];
-      if (g > constrainG || b > constrainB || r > constrainR) continue;
-      if (i > constrainHeight) continue;
+      let index = (i * img.width + j) * 4;
+
+      let r = img.pixels[index];
+      let g = img.pixels[index + 1];
+      let b = img.pixels[index + 2];
+
+      if (g > constraints[1] || b > constraints[2] || r > constraints[0])
+        continue;
+      if (i > constraints[3]) continue;
 
       let bright = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
@@ -110,16 +148,27 @@ function thresholdTritone(img, highCutoff, lowCutoff, midtone) {
 
   for (let i = 0; i < img.height; i++) {
     for (let j = 0; j < img.width; j++) {
-      let px = img.get(j, i);
-      let r = px[0];
-      let g = px[1];
-      let b = px[2];
+      let index = (i * img.width + j) * 4;
+
+      let r = img.pixels[index];
+      let g = img.pixels[index + 1];
+      let b = img.pixels[index + 2];
 
       let bright = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-      if (bright > highCutoff) img.set(j, i, color(255));
-      else if (bright < lowCutoff) img.set(j, i, color(0));
-      else img.set(j, i, midtone);
+      if (bright > highCutoff) {
+        img.pixels[index] = 255;
+        img.pixels[index + 1] = 255;
+        img.pixels[index + 2] = 255;
+      } else if (bright < lowCutoff) {
+        img.pixels[index] = 0;
+        img.pixels[index + 1] = 0;
+        img.pixels[index + 2] = 0;
+      } else {
+        img.pixels[index] = midtone[0];
+        img.pixels[index + 1] = midtone[1];
+        img.pixels[index + 2] = midtone[2];
+      }
     }
   }
 
@@ -130,11 +179,11 @@ function filterSystemOne(img) {
   tritone(img, color("#ffc414"), color("#d12a2a"), color("#6b0a59"));
   image(img, 0, 0);
   //filter(BLUR, 4);
-  halftone(img, 180, 90, 60, img.height, 0, 20, 20);
+  halftone(img, [180, 90, 60, img.height], 0, 20, 20);
 }
 
 function filterSystemTwo(img) {
-  thresholdTritone(img, 200, 100, color(255, 0, 0));
+  thresholdTritone(img, 200, 100, [255, 0, 0]);
   image(img, 0, 0);
-  halftone(img, 0, 0, 0, img.height / 2 - 100, 5, 10, 20);
+  halftone(img, [0, 0, 0, img.height / 2 - 100], 5, 10, 20);
 }
