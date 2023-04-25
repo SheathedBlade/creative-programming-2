430f1d32b342b798ca486b095b8cd5b35f45da30
Andrew Chuah <warlordpluto@gmail.com>
2023-04-25 12:51:43 -0400

    stuff

---
+++ final-project/powerup.js
@@ -0,0 +1,11 @@
+class PowerUp {
+  constructor(type) {
+    this.type = type;
+  }
+
+  update() {
+    // Check type of powerup, give appropriate powerup once player collides with it
+  }
+
+  display() {}
+}
